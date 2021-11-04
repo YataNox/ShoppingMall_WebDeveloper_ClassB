0f3c10700093d0f93c3145ef2a70d676c0e61527
YataNox <rladnwls0523@naver.com>
2021-11-04 20:22:22 +0900

    Write execute Method

--- WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/IndexAction.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/IndexAction.java
@@ -2,12 +2,14 @@ package com.ezenac.controller.action;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class IndexAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		rd.forward(request, response);
 	}
 }
