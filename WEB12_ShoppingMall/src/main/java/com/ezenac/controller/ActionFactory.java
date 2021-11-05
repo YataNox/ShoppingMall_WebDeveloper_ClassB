7dd042ac5be7ccfc4c04c01892da7133a2232e70
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-05 18:37:39 +0900

    Add command else if case - loginForm

--- WEB12_ShoppingMall/src/main/java/com/ezenac/controller/ActionFactory.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/ActionFactory.java
@@ -2,6 +2,7 @@ package com.ezenac.controller;
 
 import com.ezenac.controller.action.Action;
 import com.ezenac.controller.action.IndexAction;
+import com.ezenac.controller.action.LoginFormAction;
 
 public class ActionFactory {
 	private ActionFactory() {}
@@ -12,6 +13,7 @@ public class ActionFactory {
 		Action ac = null;
 		
 		if(command.equals("index")) ac = new IndexAction();
+		else if(command.equals("loginForm")) ac = new LoginFormAction();
 		
 		return ac;
 	}
