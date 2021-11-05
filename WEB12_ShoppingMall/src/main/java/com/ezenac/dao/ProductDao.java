35c0b2ee8952b5c84b7fe3a1dcb0aee888c3ba7a
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-05 17:38:06 +0900

    Write getNewList, getBestList Method

--- WEB12_ShoppingMall/src/main/java/com/ezenac/dao/ProductDao.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/dao/ProductDao.java
@@ -3,6 +3,11 @@ package com.ezenac.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.ezenac.dto.ProductVO;
+import com.ezenac.util.DBman;
 
 public class ProductDao {
 	private ProductDao() {}
@@ -12,4 +17,56 @@ public class ProductDao {
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+	
+	public ArrayList<ProductVO> getNewList() {
+		ArrayList<ProductVO> newlist = new ArrayList<ProductVO>();
+		String sql = "select * from new_pro_view";
+		con = DBman.getConnection();
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductVO pvo = new ProductVO();
+				// 신상품의 이름, 가격, 이미지만 현재 필요하기 때문에 필요한 필드만 dto에 담습니다.
+				pvo.setPseq(rs.getInt("pseq"));
+				pvo.setName(rs.getString("name"));
+				pvo.setPrice2(rs.getInt("price2"));
+				pvo.setImage(rs.getString("image"));
+				newlist.add(pvo);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBman.close(con, pstmt, rs);
+		}
+		
+		return newlist;
+	}
+	public ArrayList<ProductVO> getBestList() {
+		ArrayList<ProductVO> bestlist = new ArrayList<ProductVO>();
+		String sql = "select * from best_pro_view";
+		con = DBman.getConnection();
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductVO pvo = new ProductVO();
+				// 신상품의 이름, 가격, 이미지만 현재 필요하기 때문에 필요한 필드만 dto에 담습니다.
+				pvo.setPseq(rs.getInt("pseq"));
+				pvo.setName(rs.getString("name"));
+				pvo.setPrice2(rs.getInt("price2"));
+				pvo.setImage(rs.getString("image"));
+				bestlist.add(pvo);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBman.close(con, pstmt, rs);
+		}
+		return bestlist;
+	}
 }
