eada249f7dd223cc3728e1af2554ea0d459424ce
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-05 17:33:22 +0900

    Update execute Method - new,bestlist set

--- WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/IndexAction.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/IndexAction.java
@@ -1,14 +1,27 @@
 package com.ezenac.controller.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ezenac.dao.ProductDao;
+import com.ezenac.dto.ProductVO;
+
 public class IndexAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ProductDao pdao = ProductDao.getInstance();
+		
+		ArrayList<ProductVO> newlist = pdao.getNewList();
+		ArrayList<ProductVO> bestlist = pdao.getBestList();
+		
+		request.setAttribute("newProductList", newlist);
+		request.setAttribute("bestProductList", bestlist);
+		
 		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
 		rd.forward(request, response);
 	}
