cefc1cf3c6f3a4c3bba0064e91334cedca281a52
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-05 21:08:28 +0900

    Write go_next() Function

--- WEB12_ShoppingMall/src/main/webapp/member/member.js
+++ WEB12_ShoppingMall/src/main/webapp/member/member.js
@@ -11,3 +11,18 @@ function loginCheck(){
 	}
 	return true;
 }
+
+function go_next(){
+	// 자바스크립트에서 jsp페이지 내의 radio버튼을 바라볼 때, 같은 name의 radio가 여러개라면
+	// name 값에 의한 배열로 인식되어 사용됩니다.
+	// 동의함 버튼 : okon[0], 동의안함 버튼 : okon[1]
+	if(document.contractFrm.okon[1].checked == true){
+		alert("약관에 동의하셔야 회원 가입이 가능합니다.");
+	}
+	else{
+		// 스크립트 명령으로 폼의 액션 설정하고 submit 실행
+		document.contractFrm.action = "shop.do?command=joinForm";
+		document.contractFrm.submit();
+	}
+	
+}
