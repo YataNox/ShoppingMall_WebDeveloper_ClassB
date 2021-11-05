ecd5349e17a7e317343161dbcc26ee026c436dd5
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-05 18:30:38 +0900

    Create & Write OrderDao,QnaDao.java

---
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/dao/OrderDao.java
@@ -0,0 +1,15 @@
+package com.ezenac.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class OrderDao {
+	private OrderDao() {}
+	private static OrderDao ist = new OrderDao();
+	public static OrderDao getInstance() {return ist;}
+	
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+}
