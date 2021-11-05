8d99aaa7fa7f90445e16fdec3635d92d9557150c
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-05 20:45:52 +0900

    Write getMember Method

--- WEB12_ShoppingMall/src/main/java/com/ezenac/dao/MemberDao.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/dao/MemberDao.java
@@ -3,6 +3,10 @@ package com.ezenac.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.ezenac.dto.MemberVO;
+import com.ezenac.util.DBman;
 
 public class MemberDao {
 	private MemberDao() {}
@@ -12,4 +16,35 @@ public class MemberDao {
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+	
+	public MemberVO getMember(String id) {
+		MemberVO mvo = null;
+		con = DBman.getConnection();
+		String sql="select * from member where id = ?";
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				mvo = new MemberVO();
+				mvo.setId(id);
+				mvo.setPwd(rs.getString("pwd"));
+				mvo.setName(rs.getString("name"));
+				mvo.setEmail(rs.getString("email"));
+				mvo.setZip_num(rs.getString("zip_num"));
+				mvo.setAddress(rs.getString("address"));
+				mvo.setPhone(rs.getString("phone"));
+				mvo.setUseyn(rs.getString("useyn"));
+				mvo.setIndate(rs.getTimestamp("indate"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBman.close(con, pstmt, rs);
+		}
+		
+		return mvo;
+	}
 }
