30d0f6fe4a4ed0a2382ae0670af9a89353e519ee
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-05 17:31:20 +0900

    Write Prototype - Sington Pattern

--- WEB12_ShoppingMall/src/main/java/com/ezenac/dao/ProductDao.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/dao/ProductDao.java
@@ -1,5 +1,15 @@
 package com.ezenac.dao;
 
-public class ProductDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class ProductDao {
+	private ProductDao() {}
+	private static ProductDao ist = new ProductDao();
+	public static ProductDao getInstance() {return ist;}
+	
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
 }
