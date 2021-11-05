223d77bac97826a892a5dcafc42065ad0dd1c475
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-05 20:40:44 +0900

    Write execute Method

--- WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/LoginAction.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/LoginAction.java
@@ -2,12 +2,37 @@ package com.ezenac.controller.action;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ezenac.dao.MemberDao;
+import com.ezenac.dto.MemberVO;
 
 public class LoginAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String url = "member/login.jsp";
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		
+		MemberDao mdao = MemberDao.getInstance();
+		MemberVO mvo = mdao.getMember(id);
+		
+		if(mvo == null) {
+			request.setAttribute("message", "아이디가 없어요.");
+		}else if(mvo.getPwd() == null) {
+			request.setAttribute("message", "회원정보 오류. 관리자에게 문의하세요.");
+		}else if(!mvo.getPwd().equals(pwd)) {
+			request.setAttribute("message", "비밀번호가 틀려요.");
+		}else {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", mvo);
+			url="shop.do?command=index";
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
 	}
 }
