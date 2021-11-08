405f6df229431dd554400fccdc94528368ee1e3d
YataNox <rladnwls0523@naver.com>
2021-11-08 16:17:14 +0900

    Write execute Method

--- WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/IdCheckFormAction.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/IdCheckFormAction.java
@@ -2,12 +2,25 @@ package com.ezenac.controller.action;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ezenac.dao.MemberDao;
+
 public class IdCheckFormAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
 		
+		MemberDao mdao = MemberDao.getInstance();
+		int result = mdao.confirmID(id);
+		// 아이디가 사용 중이면 result : 1, 사용 가능한 아이디이면 result : -1
+		
+		request.setAttribute("result", result);
+		request.setAttribute("id", id);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("member/incheck.jsp");
+		rd.forward(request, response);
 	}
 }
