fc0f3024711af3344d25086e8f16b12544b5ab20
YataNox <rladnwls0523@naver.com>
2021-11-08 20:45:07 +0900

    Code Optimization

--- WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/FindIdPwdAction.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/FindIdPwdAction.java
@@ -1,8 +1,6 @@
 package com.ezenac.controller.action;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
