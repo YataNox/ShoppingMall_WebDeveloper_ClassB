83db4f8cc91b2f5ff54a1a01180bc8551115259a
YataNox <rladnwls0523@naver.com>
2021-11-08 18:18:53 +0900

    Write go_save function

--- WEB12_ShoppingMall/src/main/webapp/member/member.js
+++ WEB12_ShoppingMall/src/main/webapp/member/member.js
@@ -50,3 +50,28 @@ function post_zip(){
 	var opt = "toolbar=no, menubar=no, resizable=no, width=500, height=300, top=300, left=300";
 	window.open(url, "우편번호 찾기", opt);
 }
+
+function go_save(){
+	if(document.joinForm.id.value == ""){
+		alert("아이디를 입력하여 주세요.");
+		document.joinForm.id.focus();
+	}else if(document.joinForm.reid.value != document.joinForm.id.value){
+		alert("아이디 중복 확인을 하지 않았습니다.");
+		document.joinForm.id.focus();
+	}else if(document.joinForm.pwd.value == ""){
+		alert("비밀번호를 입력하여 주세요.");
+		document.joinForm.pwd.focus();
+	}else if(document.joinForm.pwd.value != document.joinForm.pwdCheck.value){
+		alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+		document.joinForm.pwd.focus();
+	}else if(document.joinForm.name.value == ""){
+		alert("이름을 입력해 주세요.");
+		document.joinForm.name.focus();
+	}else if(document.joinForm.email.value == ""){
+		alert("이메일을 입력해 주세요.");
+		document.joinForm.email.focus();
+	}else{
+		docuement.joinForm.action = "shop.do";
+		document.joinForm.submit();
+	}
+}
