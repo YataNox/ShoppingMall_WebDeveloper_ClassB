6317ce02c0ace1889e58ad071d5cf76f6e5e773c
YataNox <rladnwls0523@naver.com>
2021-11-08 17:17:40 +0900

    Write selectAddressByDong Method

--- WEB12_ShoppingMall/src/main/java/com/ezenac/dao/MemberDao.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/dao/MemberDao.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import com.ezenac.dto.AddressVO;
 import com.ezenac.dto.MemberVO;
 import com.ezenac.util.DBman;
 
@@ -70,5 +72,35 @@ public class MemberDao {
 		}
 		
 		return result;
+	}
+
+	public ArrayList<AddressVO> selectAddressByDong(String dong) {
+		ArrayList<AddressVO> list = new ArrayList<AddressVO>();
+		con = DBman.getConnection();
+		String sql="select * from address where dong like '%'||?||'%'";
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, dong);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				AddressVO avo = new AddressVO();
+				avo.setZip_num(rs.getString("zip_num"));
+				avo.setSido(rs.getString("sido"));
+				avo.setGugun(rs.getString("gugun"));
+				avo.setDong(rs.getString("dong"));
+				avo.setZip_code(rs.getString("zip_code"));
+				avo.setBunji(rs.getString("bunji"));
+				
+				list.add(avo);
+			}	
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBman.close(con, pstmt, rs);
+		}
+		
+		return list;
 	}
 }
