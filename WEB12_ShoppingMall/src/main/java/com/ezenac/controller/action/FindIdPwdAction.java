f285c7bd1362db78772b3fef7bfff0abdad24256
YataNox <rladnwls0523@naver.com>
2021-11-08 20:40:50 +0900

    Create & Write FindIdPwdAction.java

---
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/FindIdPwdAction.java
@@ -0,0 +1,19 @@
+package com.ezenac.controller.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FindIdPwdAction implements Action {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = "member/findIdPwd";
+		
+		// RequestDispatcher rd = request.getRequestDispatcher(url);
+		// rd.forward(request, response);
+		
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+}
