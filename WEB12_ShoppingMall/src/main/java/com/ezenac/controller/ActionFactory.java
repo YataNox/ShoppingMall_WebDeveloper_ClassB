bc376941df50350e97ad283e440ccd899d2afe84
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-09 19:45:18 +0900

    Add command else if case - cartList

--- WEB12_ShoppingMall/src/main/java/com/ezenac/controller/ActionFactory.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/ActionFactory.java
@@ -2,6 +2,7 @@ package com.ezenac.controller;
 
 import com.ezenac.controller.action.Action;
 import com.ezenac.controller.action.CartInsertAction;
+import com.ezenac.controller.action.CartListAction;
 import com.ezenac.controller.action.CategoryAction;
 import com.ezenac.controller.action.ContractAction;
 import com.ezenac.controller.action.FindIdFormAction;
@@ -50,10 +51,11 @@ public class ActionFactory {
 		else if(command.equals("findPwForm")) ac = new FindPwFormAction();
 		else if(command.equals("findPwStep1")) ac = new FindPwStep1Action();
 		else if(command.equals("findPwStep2")) ac = new FindPwStep2Action();
-		else if(command.equals("resetPw")) ac = new ResetPwAction();//Member End----------
+		else if(command.equals("resetPw")) ac = new ResetPwAction();
 		else if(command.equals("productDetail")) ac = new ProductDetailAction();
 		else if (command.equals("category")) ac = new CategoryAction();
 		else if(command.equals("cartInsert")) ac = new CartInsertAction();
+		else if(command.equals("cartList")) ac = new CartListAction();
 		return ac;
 	}
 }
