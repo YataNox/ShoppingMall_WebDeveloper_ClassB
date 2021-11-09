dd31ccf2343821affb8fc990e6acd7b9e9d62bc5
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-09 17:28:50 +0900

    Add request.setAttribute - id value

--- WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/FindPwFormAction.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/controller/action/FindPwFormAction.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 public class FindPwFormAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = "member/findPwForm.jsp";
+		request.setAttribute("id", request.getParameter("id"));
 		request.getRequestDispatcher(url).forward(request, response);
 	}
 }
