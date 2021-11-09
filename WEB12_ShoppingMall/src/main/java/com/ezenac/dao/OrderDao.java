1d80e88c27c938de9f04b9f792d1d2b1c5a31780
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-09 21:13:27 +0900

    Write insertOrder - Primary write

--- WEB12_ShoppingMall/src/main/java/com/ezenac/dao/OrderDao.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/dao/OrderDao.java
@@ -3,6 +3,9 @@ package com.ezenac.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.ezenac.dto.CartVO;
 
 public class OrderDao {
 	private OrderDao() {}
@@ -12,4 +15,10 @@ public class OrderDao {
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+	
+	public int insertOrder(ArrayList<CartVO> list, String id) {
+		int result = 0;
+		
+		return result;
+	}
 }
