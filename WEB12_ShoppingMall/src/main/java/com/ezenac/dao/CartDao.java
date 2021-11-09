3a6938675dae9e9aa53fee85ef6213152780374a
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-09 19:43:37 +0900

    Write insertCart Method

--- WEB12_ShoppingMall/src/main/java/com/ezenac/dao/CartDao.java
+++ WEB12_ShoppingMall/src/main/java/com/ezenac/dao/CartDao.java
@@ -3,6 +3,10 @@ package com.ezenac.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.ezenac.dto.CartVO;
+import com.ezenac.util.DBman;
 
 public class CartDao {
 	private CartDao() {}
@@ -12,4 +16,22 @@ public class CartDao {
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
+	
+	public void insertCart(CartVO cvo) {
+		String sql = "insert into cart(cseq, id, pseq, quantity) values(cart_seq.nextVal, ?, ?, ?)";
+		con = DBman.getConnection();
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, cvo.getId());
+			pstmt.setInt(2, cvo.getPseq());
+			pstmt.setInt(3, cvo.getQuantity());
+			
+			pstmt.executeUpdate();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally {
+			DBman.close(con, pstmt, rs);
+		}
+	}
 }
