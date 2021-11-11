ed0f1ff9da1043525a766cf129802f25e34ea140
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-11 19:41:19 +0900

    Write go_mov Function

--- WEB12_ShoppingMall/src/main/webapp/admin/product/product.js
+++ WEB12_ShoppingMall/src/main/webapp/admin/product/product.js
@@ -2,3 +2,7 @@ function go_wrt(){
 	document.frm.action = "shop.do?command=adminProductWriteForm";
 	document.frm.submit();
 }
+
+function go_mov(){
+	location.href="shop.do?command=adminProductList";
+}
