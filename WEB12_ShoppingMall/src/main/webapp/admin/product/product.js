f27cc1f763574591c6a7ff0bfc7755db88f9f503
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-11 19:47:23 +0900

    Write go_save Function

--- WEB12_ShoppingMall/src/main/webapp/admin/product/product.js
+++ WEB12_ShoppingMall/src/main/webapp/admin/product/product.js
@@ -6,3 +6,29 @@ function go_wrt(){
 function go_mov(){
 	location.href="shop.do?command=adminProductList";
 }
+
+function go_save(){
+	var theForm = document.frm;
+	if(theForm.kind.value==""){
+		alert("상품분류를 선택하세요.");
+		theForm.kind.focus();
+	}else if(theForm.name.value==""){
+		alert("상품명을 입력하세요.");
+		theForm.name.focus();
+	}else if(theForm.price1.value=""){
+		alert("원가를 입력하세요");
+		theForm.price1.focus();
+	}else if(theForm.price2.value=""){
+		alert("판매가를 입력하세요");
+		theForm.price2.focus();
+	}else if(theForm.content.value=""){
+		alert("상품상세를 입력하세요");
+		theForm.content.focus();
+	}else if(theForm.image.value=""){
+		alert("상품이미지를 입력하세요");
+		theForm.image.focus();
+	}else{
+		theForm.action = "shop.do?command=adminProductWrite";
+		theForm.submit();
+	}
+}
