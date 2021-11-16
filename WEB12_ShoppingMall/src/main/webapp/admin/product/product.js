716e0b69c67ff5cab9d997b3da78ad644f1e979e
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-11-16 16:37:29 +0900

    Write go_search, go_total function

--- WEB12_ShoppingMall/src/main/webapp/admin/product/product.js
+++ WEB12_ShoppingMall/src/main/webapp/admin/product/product.js
@@ -66,5 +66,20 @@ function go_mod_save(){
 			document.frm.submit();
 		}
 	}
-	
+}
+
+function go_search(){
+	if(document.frm.key.value=="")
+		return;
+		
+	var url = "shop.do?command=adminProductList&page=1";
+	// 보던 페이지가 어떤 페이지이던간에 검색 결과의 1페이지로 가기위해 파라미터 page를 1로 전송
+	document.frm.action = url;
+	document.frm.submit();
+}
+
+function go_total(){
+	document.frm.key.value="";
+	document.frm.action = "shop.do?command=adminProductList&page=1";
+	document.frm.submit();
 }
